692eb07fc94e21bfa699013728ff9ade62ddc823
rewong03 <52171617+rewong03@users.noreply.github.com>
2019-07-05 15:43:00 -0500

    Added PEP8, docstrings, argparse, logging.
    Added PEP8 formatting and docstrings for all files. 
    Replaced all print statements with logging.
    Added argparse to main.py so files can be inputted similar to xtract-tabular.

--- main.py
+++ main.py
@@ -1,56 +1,75 @@
 from location_extraction import load_city_index, load_border_index, extract_location_metadata
+import time
+import argparse
 
-''' 
+"""This script handles the run-logic for the map-wizard Skluma module. 
+The Docker container stored as a DockerHub Image should store all dependency 
+downloads. Some 'difficult' ones include the shapely and opencv (cv2) 
+downloads, Google OCR Tesseract, and libgeos-dev (C binding for shapely). 
     
-    This script handles the run-logic for the map-wizard Skluma module. The Docker container stored as a DockerHub Image
-    should store all dependency downloads. Some 'difficult' ones include the shapely and opencv (cv2) downloads, 
-    Google OCR Tesseract, and libgeos-dev (C binding for shapely). 
-    
-    This Docker Image should always run Python3.  
+This Docker Image should always run Python3.  
 
-    @ScriptAuthor: Tyler J. Skluzacek (skluzacek@uchicago.edu) --- DockerImage/Workflow point of contact
-    @ModuleAuthor: Rohan Kumar --- Module point of contact
+@ScriptAuthor: Tyler J. Skluzacek (skluzacek@uchicago.edu) --- DockerImage/Workflow point of contact
+@ModuleAuthor: Rohan Kumar --- Module point of contact
     
-'''
+"""
 
 
 def get_indices():
-    # Load city and border indices into memory.
+    """Loads city and border indexes.
+
+    Return:
+    (tuple): 2-tuple of city index and border index.
+    """
     city_index = load_city_index()
     border_index = load_border_index()
 
     return (city_index, border_index)
 
 
-# # Load file from shared volume.
-# input_file = "fix_this.png"
-#
-# # Extract map-based metadata:
-# metadata = extract_location_metadata(input_file, border_index, city_index, path_given=True, debug=False)
+def extract_map_metadata(filename, debug=False):
+    """Extracts map metadata from a map image.
 
-#Sample usage -- set debug == False to remove verbose text.
-def extract_map_metadata(filename):
+    Parameter:
+    filename (str): Path to map image.
+
+    Return:
+    metadata (tuple): 3-tuple of coordinate span of img, regions found in img,
+    and text directly found in img.
+    """
 
     indices = get_indices()
     city_index = indices[0]
     border_index = indices[1]
-    metadata = extract_location_metadata(filename, border_index, city_index,path_given=True, debug=False)
+    metadata = extract_location_metadata(filename, border_index,
+                                         city_index, path_given=True,
+                                         debug=debug)
 
-    print(metadata)
-    return(metadata)
+    return metadata
 
-import time
 
-t0 = time.time()
-extract_map_metadata('testpic.png')
-t1 = time.time()
-print(t1-t0)
+if __name__ == "__main__":
+    """Takes file paths from command line and returns metadata.
 
-""" Tyler and Rohan's 'messing around' code from 08/16/2017""" #TODO: Move logic to test-suite.
-# from contouring import isolate_text_boxes
-# import cv2
-# from PIL import Image
-#
-# boxes = isolate_text_boxes(cv2.imread(mappy), return_contours=True)
-# i = cv2.drawContours(cv2.imread(mappy), boxes, -1, (255,0,0), 2)
-# Image.fromarray(i).show()
+    Arguments:
+    --path (File path): File path of map image file.
+    --debug (bool): Whether to turn on debug mode.
+
+    Returns:
+    meta (insert type here): 3-tuple of coordinate span of img, regions found 
+    in img, and text directly found in img.
+    t1 - t0 (float): Time it took to retrieve map metadata.
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--path', help='File system path to file.',
+                        required=True)
+    parser.add_argument('--debug', help='Whether to turn on debug mode.',
+                        required=False, default=False)
+
+    args = parser.parse_args()
+    t0 = time.time()
+    meta = {"map": extract_map_metadata(args.path, args.debug)}
+    print(meta)
+    t1 = time.time()
+    print(t1 - t0)
+
